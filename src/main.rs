f99ba3fbe3f132808f63e6e43d9f911e128a0344
Veera <sveera.2001@gmail.com>
2022-12-16 16:26:49 -0500

    Get user input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use std::{
+    io::{stdout, Write},
+};
+
+fn get_input() -> String {
+    let mut input = String::new();
+    input.clear();
+    print!("calc$ ");
+    let _ = stdout().flush();
+    let _ = std::io::stdin().read_line(&mut input).unwrap();
+    let _ = input.pop();
+
+    input
+}
 
 fn main() {
-    println!("This is a calculator!");
-
+    let input = get_input();
+    println!("You entered: {input}");
 }
