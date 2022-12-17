3213be697feafffdab9d5fd48084d9931c3ef42a
Veera <sveera.2001@gmail.com>
2022-12-17 15:45:07 -0500

    Add a nice calculator user interface

--- web/main.js
+++ web/main.js
@@ -1,12 +1,28 @@
+const keys = [
+    ['C', '/', '*', 'AC'],
+    ['7', '8', '9', '-'],
+    ['4', '5', '6', '+'],
+    ['1', '2', '3', '='],
+    ['0', 'E', '.', 'E'],
+];
+
 window.onload = () => {
     const numRows = 5;
     const numCols = 4;
     const grid = document.getElementById("keyGrid");
     for (let row = 0; row < numRows; ++row) {
         for (let col = 0; col < numCols; ++col) {
-            const divEle = document.createElement("div");
-            const text = document.createTextNode("Key");
-            divEle.appendChild(text);
+            if ((row === 4 && col === 1) || (row === 4 && col === 3)) {
+                continue;
+            }
+            const divEle = document.createElement("button");
+            const text = keys[row][col];
+            if (text === '0') {
+                divEle.classList.add("item0");
+            } else if (text === '=') {
+                divEle.classList.add("itemEquals");
+            }
+            divEle.appendChild(document.createTextNode(text));
             grid.appendChild(divEle);
         }
     }
